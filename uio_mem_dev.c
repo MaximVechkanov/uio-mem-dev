4bbe3afe33287f1fed51426353c4cf92bf3d0861
Maxim Vechkanov <maxim.vechkanov@radiogigabit.com>
2023-05-21 21:59:54 +0300

    Support multiple devices

--- uio_mem_dev.c
+++ uio_mem_dev.c
@@ -10,26 +10,27 @@
 
 #define CLASS_NAME "uio_logical_mem"
 #define DEVICE_NAME "uio_mem_dev"
+#define LOCAL_MAX_DEVICES 10
 
-static struct 
+static struct
 {
-	struct uio_info * info;
-	struct class * uio_mem_class;
-	struct device * dev;
-	dev_t devt;
+	struct uio_info *info[LOCAL_MAX_DEVICES];
+	struct class *uio_mem_class;
+	struct device *dev[LOCAL_MAX_DEVICES];
+	uint32_t numDevices;
+	dev_t major;
+	dev_t minor_start;
+
 } moduleData;
 
 int init_module(void)
 {
 	int rc;
+	uint32_t devIdx;
+	dev_t devt;
+	char buf[64];
 
-	printk(KERN_INFO "Hello world!\n");
-
-	moduleData.info = kmalloc(sizeof(struct uio_info), GFP_KERNEL);
-	if (!moduleData.info)
-		return -ENOMEM;
-	else
-		printk(KERN_INFO "UIO info allocated\n");
+	memset(&moduleData, 0, sizeof(moduleData));
 
 	moduleData.uio_mem_class = class_create(THIS_MODULE, CLASS_NAME);
 	if (!moduleData.uio_mem_class)
@@ -37,51 +38,85 @@ int init_module(void)
 		printk(KERN_ERR "Failed to class_create()");
 		return -1;
 	}
-	
-	rc = alloc_chrdev_region(&moduleData.devt, 0, 1, DEVICE_NAME);
+
+	rc = alloc_chrdev_region(&devt, 0, LOCAL_MAX_DEVICES, DEVICE_NAME);
 	if (rc != 0)
 	{
 		printk(KERN_ERR "Failed to alloc_chrdev_region: %d", rc);
-		return -1;
+		goto class_created;
 	}
 
-	moduleData.dev = device_create(moduleData.uio_mem_class, NULL, moduleData.devt, NULL, DEVICE_NAME);
-	if (!moduleData.dev)
+	moduleData.major = MAJOR(devt);
+	moduleData.minor_start = MINOR(devt);
+
+	for (devIdx = 0; devIdx < 5; ++devIdx)
 	{
-		printk(KERN_ERR "Failed to device_create()");
-		return -1;
-	}
+		moduleData.dev[devIdx] = device_create(moduleData.uio_mem_class, NULL, MKDEV(moduleData.major, moduleData.minor_start + devIdx), NULL, DEVICE_NAME "%u", devIdx);
+		if (!moduleData.dev[devIdx])
+		{
+			printk(KERN_ERR "Failed to device_create()");
+			return -1;
+		}
 
-	moduleData.info->name = DEVICE_NAME;
-	moduleData.info->version = "0.1.0";
-	moduleData.info->irq = UIO_IRQ_NONE;
-	
-	moduleData.info->mem[0].name = "main";
-	moduleData.info->mem[0].memtype = UIO_MEM_LOGICAL;
-	moduleData.info->mem[0].addr = (phys_addr_t)(void *)get_zeroed_page(GFP_KERNEL | __GFP_COMP);
-	moduleData.info->mem[0].size = PAGE_SIZE;
+		moduleData.info[devIdx] = kmalloc(sizeof(struct uio_info), GFP_KERNEL);
+		if (!moduleData.info[devIdx])
+			return -ENOMEM;
+		// else
+		// 	printk(KERN_INFO "UIO info allocated\n");
 
-	rc = uio_register_device(moduleData.dev, moduleData.info);
-	if (rc != 0)
-	{
-		printk(KERN_WARNING "Failed to register UIO: %d\n", rc);
-		return rc;
+		memset(moduleData.info[devIdx], 0, sizeof(struct uio_info));
+
+		snprintf(buf, sizeof(buf), DEVICE_NAME "_%u", devIdx);
+
+		moduleData.info[devIdx]->name = buf;
+		moduleData.info[devIdx]->version = "0.1.0";
+		moduleData.info[devIdx]->irq = UIO_IRQ_NONE;
+
+		moduleData.info[devIdx]->mem[0].name = "main";
+		moduleData.info[devIdx]->mem[0].memtype = UIO_MEM_LOGICAL;
+		moduleData.info[devIdx]->mem[0].addr = (phys_addr_t)(void *)get_zeroed_page(GFP_KERNEL | __GFP_COMP);
+		moduleData.info[devIdx]->mem[0].size = PAGE_SIZE;
+
+		rc = uio_register_device(moduleData.dev[devIdx], moduleData.info[devIdx]);
+		if (rc != 0)
+		{
+			printk(KERN_WARNING "Failed to register UIO: %d\n", rc);
+			return rc;
+		}
+		else
+			printk(KERN_INFO "UIO device registered\n");
+
+		++moduleData.numDevices;
 	}
-	else
-		printk(KERN_INFO "UIO device registered\n");
 
 	return 0;
+
+// device_created:
+
+// 	device_destroy(moduleData.uio_mem_class, moduleData.dev->devt);
+
+class_created:
+
+	class_destroy(moduleData.uio_mem_class);
+
+	return rc;
 }
 
 void cleanup_module(void)
 {
-	uio_unregister_device(moduleData.info);
-	free_page(moduleData.info->mem[0].addr);
-	kfree(moduleData.info);
+	uint32_t devIdx;
+	for (devIdx = 0; devIdx < moduleData.numDevices; ++devIdx)
+	{
+		uio_unregister_device(moduleData.info[devIdx]);
+		free_page(moduleData.info[devIdx]->mem[0].addr);
+		kfree(moduleData.info[devIdx]);
+		device_destroy(moduleData.uio_mem_class, moduleData.dev[devIdx]->devt);
+	}
 
-	device_destroy(moduleData.uio_mem_class, moduleData.dev->devt);
+	moduleData.numDevices = 0;
+
 	class_destroy(moduleData.uio_mem_class);
-	unregister_chrdev_region(moduleData.devt, 1);
+	unregister_chrdev_region(MKDEV(moduleData.major, moduleData.minor_start), LOCAL_MAX_DEVICES);
 
 	printk(KERN_INFO "Bye Bye cruel world\n");
 }
